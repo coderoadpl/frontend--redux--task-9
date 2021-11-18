796abf3ec3ab94770ca24dec07504940b2d4ad11
Mateusz Choma <kontakt@coderoad.pl>
2021-11-18 13:49:53 +0100

    Rendering all elements in `ToDo` component

--- src/ToDo.js
+++ src/ToDo.js
@@ -1,9 +1,70 @@
 import React from 'react'
 
 export const ToDo = () => {
+  const tasks = [
+    {
+      id: 0,
+      text: 'Wynieś śmieci',
+      isCompleted: false
+    },
+    {
+      id: 1,
+      text: 'Zmyj naczynia',
+      isCompleted: true
+    }
+  ]
+
   return (
-    <div>
-      TODO
+    <div
+      style={{
+        width: 320,
+        margin: '0 auto'
+      }}
+    >
+      <form
+        style={{
+          width: '100%',
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}
+      >
+        <input
+          placeholder={'Type new to do item...'}
+        />
+        <button>
+          ADD
+        </button>
+      </form>
+      <ol>
+        {
+          tasks && tasks.map((task) => {
+            return (
+              <li
+                key={task.id}
+              >
+                <div
+                  style={{
+                    width: '100%',
+                    display: 'flex',
+                    justifyContent: 'space-between'
+                  }}
+                >
+                  <span
+                    style={{
+                      textDecoration: task.isCompleted ? 'line-through' : 'none'
+                    }}
+                  >
+                    {task.text}
+                  </span>
+                  <button>
+                    DELETE
+                  </button>
+                </div>
+              </li>
+            )
+          })
+        }
+      </ol>
     </div>
   )
 }
