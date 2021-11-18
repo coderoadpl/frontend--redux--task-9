fbab495efd0511934efbfaf480f7323900ffdec5
Mateusz Choma <kontakt@coderoad.pl>
2021-11-18 11:32:24 +0100

    Headless counter created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-console.log('Hello!')
+console.log('Hello in headless counter app!')
+
+const state = {
+  number: 0
+}
+
+const inc = () => {
+  state.number = state.number + 1
+  render()
+}
+const dec = () => {
+  state.number = state.number - 1
+  render()
+}
+
+const render = () => {
+  console.log(`Current number is ${state.number}`)
+}
+
+window.inc = inc
+window.dec = dec
+
+render()
