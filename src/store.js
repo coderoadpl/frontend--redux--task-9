6c7b224e910471e75ea0beee2ddfabe7d86685ad
Mateusz Choma <kontakt@coderoad.pl>
2021-11-18 14:33:03 +0100

    Using `redux` `combineReducers` function

--- src/store.js
+++ src/store.js
@@ -1,24 +1,24 @@
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 
 import counterReducer from './state/counter'
 import todoReducer from './state/todo'
 
-// const rootReducer = combineReducers({
-//   counter: counterReducer,
-//   todo: todoReducer
-// })
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  todo: todoReducer
+})
 
-const rootInitialState = {
-  counter: counterReducer(undefined, { type: '@@INIT' }),
-  todo: todoReducer(undefined, { type: '@@INIT' })
-}
+// const rootInitialState = {
+//   counter: counterReducer(undefined, { type: '@@INIT' }),
+//   todo: todoReducer(undefined, { type: '@@INIT' })
+// }
 
-const rootReducer = (state = rootInitialState, action) => {
-  return {
-    counter: counterReducer(state.counter, action),
-    todo: todoReducer(state.todo, action)
-  }
-}
+// const rootReducer = (state = rootInitialState, action) => {
+//   return {
+//     counter: counterReducer(state.counter, action),
+//     todo: todoReducer(state.todo, action)
+//   }
+// }
 
 export const store = createStore(
   rootReducer,
