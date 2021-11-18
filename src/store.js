2b01f5f777cffc39775d9c85e1b7b12d710875be
Mateusz Choma <kontakt@coderoad.pl>
2021-11-18 14:42:42 +0100

    Preloading and saving state

--- src/store.js
+++ src/store.js
@@ -3,24 +3,22 @@ import { createStore, combineReducers } from 'redux'
 import counterReducer from './state/counter'
 import todoReducer from './state/todo'
 
+const LOCAL_STORAGE_KEY = 'coderaod-redux-task-9'
+
 const rootReducer = combineReducers({
   counter: counterReducer,
   todo: todoReducer
 })
 
-// const rootInitialState = {
-//   counter: counterReducer(undefined, { type: '@@INIT' }),
-//   todo: todoReducer(undefined, { type: '@@INIT' })
-// }
-
-// const rootReducer = (state = rootInitialState, action) => {
-//   return {
-//     counter: counterReducer(state.counter, action),
-//     todo: todoReducer(state.todo, action)
-//   }
-// }
+const preloadedState = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY)) || undefined
 
 export const store = createStore(
   rootReducer,
+  preloadedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
+
+store.subscribe(() => {
+  const state = store.getState()
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(state))
+})
