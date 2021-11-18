6c925b8fa2610ceab4450246d928e4e0c8dd6cd7
Mateusz Choma <kontakt@coderoad.pl>
2021-11-18 13:20:43 +0100

    Basic and simplest Reaact integration

--- src/Counter.js
+++ src/Counter.js
@@ -1,33 +1,38 @@
 import React from 'react'
 
 import { store } from './store'
-
 import { createActionInc, createActionDec } from './state/counter'
-
-console.log('Hello in headless counter app!')
 
 const incHandler = (delta = 1) => { store.dispatch(createActionInc(delta)) }
 const decHandler = (delta = 1) => { store.dispatch(createActionDec(delta)) }
 
-const listener = () => render()
+export const Counter = () => {
+  const [, forceUpdate] = React.useState(false)
 
-store.subscribe(listener)
-
-const render = () => {
   const currentState = store.getState()
   const { number } = currentState
-  console.log(`Current number is ${number}`)
-}
 
-window.inc = incHandler
-window.dec = decHandler
+  React.useEffect(() => {
+    const listener = () => forceUpdate((prevState) => !prevState)
 
-render()
+    store.subscribe(listener)
+  }, [])
 
-export const Counter = () => {
   return (
     <div>
-      COUNTER
+      <h1>
+        { number }
+      </h1>
+      <button
+        onClick={() => incHandler()}
+      >
+        +
+      </button>
+      <button
+        onClick={() => decHandler()}
+      >
+        -
+      </button>
     </div>
   )
 }
